cbfcd3b16341c361a02012e25562e64e98dc60e0
josephc2195 <chicaj1221@gmail.com>
2021-05-18 19:16:04 -0400

    Config file

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 import sys
+import config
 from twitterBot import TwitterBot
 
-credentials = {
-    "CONSUMER_KEY": "ExHzjjl1dSjff4rNMDMEaVSRk",
-    "CONSUMER_SECRET": "CqzcyIJXfkoZtfol8qXgcLCDQ7BKjqCCAKX7iWiA3FFwTMfzpi",
-    "ACCESS_TOKEN": "1241156834959994884-YH7zjfDITY95N5a0Q5FE2a40Ylf6lO",
-    "ACCESS_TOKEN_SECRET": "vy4NjMY4KTwyJxtlyuYwFttRFdqv2DbxPd28fPhNsVa3D"
-}
 
-bot = TwitterBot(credentials)
+bot = TwitterBot(config.credentials)
 
 if sys.argv[1] == "tweet":
     bot.tweet(sys.argv[2])
